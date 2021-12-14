b87091b7b9d12f411dd3ab6ae424856d6781ea74
gandalfgreyhem5070 <ashleyaritchie@gmail.com>
2021-12-14 11:20:21 -0500

    corrected issues to App.js myMessages.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
         <ChatEngine 
             height = "100vh"
             projectID= "2ec540f7-9d98-4a2c-80b2-26feaf4c5dac"
-            userName="javascriptmastery"
+            userName="gandalfgreyhem"
             userSecret="123123"
             renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
             />
--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,9 +1,24 @@
-const MyMess = () => {
+import MessageForm from "./MessageForm";
+
+const MyMessage = ({ message }) => {
+    // checks if the message is text or a image
+    //and renders image
+    if(message?.attachments?.length > 0){
     return (
-        <div>
-            MessageForm
+        <img
+            src={message.attachments[0].file}
+            alt="message-attachment"
+            className="message-image"
+            style= {{float: 'right'}}
+        />
+    );   
+}
+// If not an image the message is rendered
+    return (
+        <div className='message' style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50'}}>
+            {message.text}
         </div>
     );
 }
 
-export default MessageForm;
+export default MyMessage;
